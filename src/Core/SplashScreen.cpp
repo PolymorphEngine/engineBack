07aeaa77ea8879424bf470b1efece0abcbc13cd9
Tristan <tristan.gremont@epitech.eu>
2022-10-05 21:26:51 +0100

    fix: fixed some base compile error

--- src/Core/SplashScreen.cpp
+++ src/Core/SplashScreen.cpp
@@ -19,6 +19,7 @@ Polymorph::SplashScreen::SplashScreen(std::string path)
     if (path.ends_with(R"(\Game\Assets)"))
         path = path.substr(0, path.size() - std::string(R"(\Game\Assets)").size());
     _icon = std::make_unique<TextureModule>("./"+path+"/Engine/PolymorphEngineIcon.png");
+    _icon->setPosition(Vector2(0.35f * GraphicalAPI::CurrentDisplay->getResolution().x,
                               0.35f * GraphicalAPI::CurrentDisplay->getResolution().y));
     _label->setPosition(Vector2(0.35f * GraphicalAPI::CurrentDisplay->getResolution().x,
                                 0.65f * GraphicalAPI::CurrentDisplay->getResolution().y));
