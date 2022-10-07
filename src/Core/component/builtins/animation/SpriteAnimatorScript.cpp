3067f8a372097efaf723000d3e01c1f55601d005
Tristan <tristan.gremont@epitech.eu>
2022-10-07 22:11:15 +0100

    fix: wrong include path and secured sprite animator "startAnimation"

--- src/Core/component/builtins/animation/SpriteAnimatorScript.cpp
+++ src/Core/component/builtins/animation/SpriteAnimatorScript.cpp
@@ -33,8 +33,10 @@ namespace Polymorph
 
     void SpriteAnimatorComponent::startAnimation(std::string animationName)
     {
+        if (animations.empty())
+            return;
         //TODO(ERR): possible failure
-        if (!animationName.empty() && !animations.empty() && animations.size() > 1)
+        if (!animationName.empty() && animations.size() > 1)
             _currentAnimation = std::find_if(animations.begin(), animations.end(), [&animationName](std::shared_ptr<SpriteAnimation> x){
                 return x->animationName == animationName;
             }).operator*();
