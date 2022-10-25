5ff56622eaafa8a3decf20121cb3fa5a91e03895
Tristan Gremont <tristan.gremont@epitech.eu>
2022-10-25 22:24:45 +0200

    fix: asset manager wasn't also checking that the path wasn't just a directory + sanitized path

--- src/Plugins/AssetManager.cpp
+++ src/Plugins/AssetManager.cpp
@@ -12,7 +12,7 @@ std::string AssetManager::tryResolve(const std::string &resource)
 {
     for (auto &folder: _assetsFolders) {
         std::string path = folder + "/" + resource;
-        if (std::filesystem::exists(path))
+        if (std::filesystem::exists(path) && !std::filesystem::is_directory(path))
             return path;
     }
     return "";
@@ -20,5 +20,12 @@ std::string AssetManager::tryResolve(const std::string &resource)
 
 void AssetManager::addPath(const std::string &path)
 {
-    _assetsFolders.push_back(path);
+    std::string p;
+    if (path[path.size() - 1] == '/')
+        p = path.substr(0, path.size() - 1);
+    else
+        p = path;
+    while(p.find("//") != std::string::npos)
+        p.replace(p.find("//"), 2, "/");
+    _assetsFolders.push_back(p);
 }
